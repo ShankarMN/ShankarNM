bda5d911911863b271b1033b1b83aaf420a44063
smeharwa <smeharwa@SMEHARWA-6WBB6.partnet.cisco.com>
2018-05-03 09:10:17 +0530

    third commit

---
+++ PayTM/src/com/one97comm/paytmapp/login/Login.java
@@ -0,0 +1,10 @@
+package com.one97comm.paytmapp.login;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// System.out.println();// write sysout and then control + space will
+		//give// the code recommendations
+		System.out.println("Hello"); 
+	}
+}
